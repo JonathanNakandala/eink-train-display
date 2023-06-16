2a7335b8f137faee1dbdb433f46f2c317afbf5b9
Jonathan Nakandala <jonathannakandala@gmail.com>
2023-06-16 12:44:30 +0100

    Reformat main.py with black

--- main.py
+++ main.py
@@ -23,56 +23,67 @@ from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
 
 config = configparser.ConfigParser()
-config.read(os.path.join(Path(__file__).parent, 'configuration.ini'))
+config.read(os.path.join(Path(__file__).parent, "configuration.ini"))
 stations = {
-    'North': {
-        'from': config['stations']['northbound_from'],
-        'to': config['stations']['northbound_to']
+    "North": {
+        "from": config["stations"]["northbound_from"],
+        "to": config["stations"]["northbound_to"],
     },
-    'South': {
-        'from': config['stations']['southbound_from'],
-        'to': config['stations']['sountbound_to']
-    }
+    "South": {
+        "from": config["stations"]["southbound_from"],
+        "to": config["stations"]["sountbound_to"],
+    },
 }
 
-LDB_TOKEN = config['tokens']['national_rail']
-WSDL = 'http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01'
+LDB_TOKEN = config["tokens"]["national_rail"]
+WSDL = "http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01"
 history = HistoryPlugin()
 client = Client(wsdl=WSDL, plugins=[history])
 header = xsd.Element(
-    '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}AccessToken',
-    xsd.ComplexType([
-        xsd.Element(
-            '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}TokenValue',
-            xsd.String()),
-    ])
+    "{http://thalesgroup.com/RTTI/2013-11-28/Token/types}AccessToken",
+    xsd.ComplexType(
+        [
+            xsd.Element(
+                "{http://thalesgroup.com/RTTI/2013-11-28/Token/types}TokenValue",
+                xsd.String(),
+            ),
+        ]
+    ),
 )
 header_value = header(TokenValue=LDB_TOKEN)
 
 
 def getDepartures(numRows, atStation, toStation):
     response = client.service.GetDepartureBoard(
-        numRows=numRows, crs=atStation, filterCrs=toStation, _soapheaders=[header_value])
+        numRows=numRows, crs=atStation, filterCrs=toStation, _soapheaders=[header_value]
+    )
     return response
 
 
-fontdir = os.path.join(Path(__file__).parent, 'fonts')
+fontdir = os.path.join(Path(__file__).parent, "fonts")
 logging.basicConfig(level=logging.DEBUG)
-print(os.path.join(fontdir, 'Overpass/Overpass-ExtraLight.ttf'))
-font_time = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-ExtraLight.ttf'), 95)
-font_direction = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-Light.ttf'), 23)
-font_traininfo = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-Light.ttf'), 15)
-font_dayofweek = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-SemiBold.ttf'), 65)
-font_date = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-ExtraLight.ttf'), 65)
-font_bigtemp = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-ExtraLight.ttf'), 75)
-font_smalltemp = ImageFont.truetype(os.path.join(
-    fontdir, 'Overpass/Overpass-ExtraLight.ttf'), 27)
+print(os.path.join(fontdir, "Overpass/Overpass-ExtraLight.ttf"))
+font_time = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-ExtraLight.ttf"), 95
+)
+font_direction = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-Light.ttf"), 23
+)
+font_traininfo = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-Light.ttf"), 15
+)
+font_dayofweek = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-SemiBold.ttf"), 65
+)
+font_date = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-ExtraLight.ttf"), 65
+)
+font_bigtemp = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-ExtraLight.ttf"), 75
+)
+font_smalltemp = ImageFont.truetype(
+    os.path.join(fontdir, "Overpass/Overpass-ExtraLight.ttf"), 27
+)
 
 
 def drawDeparturesIssue(draw, y, departures):
@@ -82,7 +93,7 @@ def drawDeparturesIssue(draw, y, departures):
     except:
         return 0
     # The message is long with a link to their website, so split it at the .
-    message = message.split('.', 1)[0]
+    message = message.split(".", 1)[0]
     lines = textwrap.wrap(message, width=50)
     print(message)
     # draw.text((train_columns[0], y), message,  font = font_traininfo, fill = 0)
@@ -105,28 +116,31 @@ def drawDepartures(draw, y, now, departures):
     try:
         services = departures.trainServices.service
     except:
-        drawDeparturesIssue(draw, y,  departures)
+        drawDeparturesIssue(draw, y, departures)
         return 0
 
     for service in services:
-        now_hours = datetime.strptime(now.strftime("%H:%M"), '%H:%M')
-        arrival_time = datetime.strptime(service.std, '%H:%M')
+        now_hours = datetime.strptime(now.strftime("%H:%M"), "%H:%M")
+        arrival_time = datetime.strptime(service.std, "%H:%M")
         time_until = arrival_time - now_hours
         time_until_minutes = int(time_until.total_seconds() / 60)
         if time_until_minutes > 60:
             hours, minutes = divmod(time_until_minutes, 60)
-            time_until_text = f'{hours} HR {minutes} MINS'
+            time_until_text = f"{hours} HR {minutes} MINS"
         else:
-            time_until_text = f'{time_until_minutes} MINS'
+            time_until_text = f"{time_until_minutes} MINS"
         print(time_until_minutes)
-        draw.text((train_columns[0], y), service.std,
-                  font=font_traininfo, fill=0)
-        draw.text((train_columns[1], y), service.destination.location[0].locationName.upper(
-        ),  font=font_traininfo, fill=0)
-        draw.text((train_columns[2], y), service.etd.upper(
-        ),  font=font_traininfo, fill=0)
-        draw.text((train_columns[3], y), time_until_text,
-                  font=font_traininfo, fill=0)
+        draw.text((train_columns[0], y), service.std, font=font_traininfo, fill=0)
+        draw.text(
+            (train_columns[1], y),
+            service.destination.location[0].locationName.upper(),
+            font=font_traininfo,
+            fill=0,
+        )
+        draw.text(
+            (train_columns[2], y), service.etd.upper(), font=font_traininfo, fill=0
+        )
+        draw.text((train_columns[3], y), time_until_text, font=font_traininfo, fill=0)
         y += train_line_offset
 
 
@@ -144,18 +158,18 @@ def getWeather():
     """
     endpoint = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        "id": config['weather']['townID'],
+        "id": config["weather"]["townID"],
         "units": "metric",
-        "APPID": config['tokens']['open_weather_map']
+        "APPID": config["tokens"]["open_weather_map"],
     }
     resp = requests.get(url=endpoint, params=params)
     data = resp.json()
     print(data)
     temp = {
-        "Average": roundThenString(data['main']['temp']),
-        "High": roundThenString(data['main']['temp_max']),
-        "Low": roundThenString(data['main']['temp_min']),
-        "Weather": data['weather'][0]['main']
+        "Average": roundThenString(data["main"]["temp"]),
+        "High": roundThenString(data["main"]["temp_max"]),
+        "Low": roundThenString(data["main"]["temp_min"]),
+        "Weather": data["weather"][0]["main"],
     }
     return temp
 
@@ -197,25 +211,31 @@ def drawTemp(image, draw, tempEndPosition):
     temp = getWeather()
     hightext = f'{temp["High"]}°C'
     lowtext = f'{temp["Low"]}°C'
-    draw.text((smallTempPosition(draw, hightext, tempEndPosition), 365),
-              hightext, font=font_smalltemp, fill=0)
-    draw.text((smallTempPosition(draw, lowtext, tempEndPosition), 400),
-              lowtext, font=font_smalltemp, fill=0)
+    draw.text(
+        (smallTempPosition(draw, hightext, tempEndPosition), 365),
+        hightext,
+        font=font_smalltemp,
+        fill=0,
+    )
+    draw.text(
+        (smallTempPosition(draw, lowtext, tempEndPosition), 400),
+        lowtext,
+        font=font_smalltemp,
+        fill=0,
+    )
     draw.text((560, 352), f'{temp["Average"]}°C', font=font_bigtemp, fill=0)
-    drawWeather(image, temp['Weather'])
+    drawWeather(image, temp["Weather"])
 
 
 def main():
     try:
-
         epd = epd7in5_V2.EPD()
         logging.info("Initialising the display...")
         epd.init()
         # epd.Clear()
 
         # Create blank monochrome image to draw onto
-        Himage = Image.new('1', (epd.width, epd.height),
-                           255)  # 255: clear the frame
+        Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(Himage)
         logging.info("Drawing the time")
         now = datetime.now()
@@ -231,25 +251,35 @@ def main():
         length_date = draw.textsize(dt_date, font=font_date)[0]
         length_month = draw.textsize(dt_month, font=font_date)[0]
         dayofweek_start_position = 428
-        date_start_position = dayofweek_start_position+length_dayofweek+10
-        month_start_position = date_start_position+length_date+10
-        date_x_positions = [dayofweek_start_position,
-                            date_start_position, month_start_position]
-        draw.text((date_x_positions[0], date_y),
-                  dt_dayofweek, font=font_dayofweek, fill=0)
-        draw.text((date_x_positions[1], date_y),
-                  dt_date, font=font_date, fill=0)
-        draw.text((date_x_positions[2], date_y),
-                  dt_month, font=font_date, fill=0)
+        date_start_position = dayofweek_start_position + length_dayofweek + 10
+        month_start_position = date_start_position + length_date + 10
+        date_x_positions = [
+            dayofweek_start_position,
+            date_start_position,
+            month_start_position,
+        ]
+        draw.text(
+            (date_x_positions[0], date_y), dt_dayofweek, font=font_dayofweek, fill=0
+        )
+        draw.text((date_x_positions[1], date_y), dt_date, font=font_date, fill=0)
+        draw.text((date_x_positions[2], date_y), dt_month, font=font_date, fill=0)
 
         logging.info("Drawing the Train Arrivals")
-        draw.text((60, 168), 'NORTHBOUND', font=font_direction, fill=0)
-        drawDepartures(draw, 210, now, getDepartures(
-            4, stations['North']['from'], stations['North']['to']))
-        draw.text((60, 312), 'SOUTHBOUND', font=font_direction, fill=0)
-        drawDepartures(draw, 356, now, getDepartures(
-            4, stations['South']['from'], stations['South']['to']))
-        drawTemp(Himage, draw, month_start_position+length_month)
+        draw.text((60, 168), "NORTHBOUND", font=font_direction, fill=0)
+        drawDepartures(
+            draw,
+            210,
+            now,
+            getDepartures(4, stations["North"]["from"], stations["North"]["to"]),
+        )
+        draw.text((60, 312), "SOUTHBOUND", font=font_direction, fill=0)
+        drawDepartures(
+            draw,
+            356,
+            now,
+            getDepartures(4, stations["South"]["from"], stations["South"]["to"]),
+        )
+        drawTemp(Himage, draw, month_start_position + length_month)
 
         logging.info("Displaying image and saving preview.png")
         Himage.save("preview.png")
