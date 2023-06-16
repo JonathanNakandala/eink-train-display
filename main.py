9ee457c07301562c7fb86741e88a3d157b11593c
Jonathan Nakandala <jonathannakandala@gmail.com>
2023-06-16 15:39:18 +0100

    Fix most linting issues

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Home Dashboard for a WaveShare 7.5 e-ink display with Raspberry PI
 import configparser
 import logging
 import os
+import sys
 import textwrap
 from datetime import datetime
 from pathlib import Path
@@ -15,7 +16,7 @@ from svglib.svglib import svg2rlg
 from zeep import Client, xsd
 from zeep.plugins import HistoryPlugin
 
-from waveshare_epd import epd7in5_V2
+from waveshare_epd import epd7in5_V2, epdconfig
 
 config = configparser.ConfigParser()
 config.read(os.path.join(Path(__file__).parent, "configuration.ini"))
@@ -95,20 +96,31 @@ font_smalltemp = ImageFont.truetype(
 )
 
 
-def drawDeparturesIssue(draw, y, departures):
-    # Either return blank or display engineering message
+def draw_departure_issues(draw, y_position, departures):
+    """
+    Either return blank or display engineering message when no results
+
+    Args:
+        draw: _description_
+        y: _description_
+        departures: _description_
+
+    Returns:
+        _description_
+    """
+    #
     try:
-        message = departures.nrccMessages.message[0]._value_1
-    except:
+        message = departures.nrccMessages.message[0]._value_1  # pylint: disable=W0212
+    except KeyError:
         return 0
     # The message is long with a link to their website, so split it at the .
     message = message.split(".", 1)[0]
     lines = textwrap.wrap(message, width=50)
     print(message)
     # draw.text((train_columns[0], y), message,  font = font_traininfo, fill = 0)
-    y_text = y
+    y_text = y_position
     for line in lines:
-        width, height = draw.textsize(line, font=font_traininfo)
+        _, height = draw.textsize(line, font=font_traininfo)
         draw.text(((train_columns[0]), y_text), line, font=font_traininfo)
         y_text += height
 
@@ -118,14 +130,26 @@ train_columns[0] = 60
 train_columns[1] = train_columns[0] + 73
 train_columns[2] = train_columns[1] + 167
 train_columns[3] = train_columns[2] + 103
-train_line_offset = 19
+TRAIN_LINE_OFFSET = 19
 
 
-def drawDepartures(draw, y, now, departures):
+def draw_departures(draw, y_position, now, departures):
+    """
+    Draw the Train Departures Information
+
+    Args:
+        draw: _description_
+        y: _description_
+        now: _description_
+        departures: _description_
+
+    Returns:
+        _description_
+    """
     try:
         services = departures.trainServices.service
-    except:
-        drawDeparturesIssue(draw, y, departures)
+    except KeyError:
+        draw_departure_issues(draw, y_position, departures)
         return 0
 
     for service in services:
@@ -139,18 +163,25 @@ def drawDepartures(draw, y, now, departures):
         else:
             time_until_text = f"{time_until_minutes} MINS"
         print(time_until_minutes)
-        draw.text((train_columns[0], y), service.std, font=font_traininfo, fill=0)
         draw.text(
-            (train_columns[1], y),
+            (train_columns[0], y_position), service.std, font=font_traininfo, fill=0
+        )
+        draw.text(
+            (train_columns[1], y_position),
             service.destination.location[0].locationName.upper(),
             font=font_traininfo,
             fill=0,
         )
         draw.text(
-            (train_columns[2], y), service.etd.upper(), font=font_traininfo, fill=0
+            (train_columns[2], y_position),
+            service.etd.upper(),
+            font=font_traininfo,
+            fill=0,
         )
-        draw.text((train_columns[3], y), time_until_text, font=font_traininfo, fill=0)
-        y += train_line_offset
+        draw.text(
+            (train_columns[3], y_position), time_until_text, font=font_traininfo, fill=0
+        )
+        y_position += TRAIN_LINE_OFFSET
 
 
 def round_number_to_string(number: int) -> str:
@@ -286,6 +317,7 @@ def draw_temp(image, draw: ImageDraw, temp_end_position):
 
 
 def main():
+    """Program Entrypoint"""
     try:
         epd = epd7in5_V2.EPD()
         logging.info("Initialising the display...")
@@ -324,14 +356,14 @@ def main():
 
         logging.info("Drawing the Train Arrivals")
         draw.text((60, 168), "NORTHBOUND", font=font_direction, fill=0)
-        drawDepartures(
+        draw_departures(
             draw,
             210,
             now,
             get_departures(4, stations["North"]["from"], stations["North"]["to"]),
         )
         draw.text((60, 312), "SOUTHBOUND", font=font_direction, fill=0)
-        drawDepartures(
+        draw_departures(
             draw,
             356,
             now,
@@ -350,8 +382,8 @@ def main():
 
     except KeyboardInterrupt:
         logging.info("ctrl + c:")
-        epd7in5.epdconfig.module_exit()
-        exit()
+        epdconfig.RaspberryPi().module_exit()
+        sys.exit()
 
 
 main()
