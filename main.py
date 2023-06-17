69ff8c480eee4e038fef26c7a722b2e2e31e99a7
Jonathan Nakandala <jonathannakandala@gmail.com>
2023-06-18 00:12:00 +0100

    Refactor into a server with periodic 5 minute updates

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 Home Dashboard for a WaveShare 7.5 e-ink display with Raspberry PI
 """
 import sys
+import datetime
 
+import httpx
 import structlog
+from PIL import Image
+import uvicorn
+from pydantic import BaseModel
+from fastapi import Depends, FastAPI, BackgroundTasks, HTTPException
 
 from sources import NationalRail, Weather
 from render import Pillow
@@ -15,6 +21,40 @@ log = structlog.get_logger()
 
 config = load_config()
 
+app = FastAPI()
+
+dashboard_update_interval = datetime.timedelta(minutes=5)
+dashboard_update_enabled_hours = (6, 24)  # Update enabled between 6 AM and 11 PM
+
+
+class APIConfig:
+    """
+    Singleton for API Configuration
+    """
+
+    _instance = None
+    update_interval: datetime.timedelta = datetime.timedelta(minutes=5)
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(APIConfig, cls).__new__(cls)
+        return cls._instance
+
+
+def get_apiconfig():
+    """
+    Returns APIConfig for use in Depends
+    """
+    return APIConfig()
+
+
+class UpdateConfig(BaseModel):
+    """
+    Pydantic Model for API Config
+    """
+
+    interval_minutes: int
+
 
 def round_number_to_string(number: int) -> str:
     """Round an int and convert to string"""
@@ -24,7 +64,6 @@ def round_number_to_string(number: int) -> str:
 def get_weather():
     """
     Get Weather from Open Weather Map API
-
     """
     client = Weather.OpenWeather(token=config.tokens.open_weather_map)
     data = client.get_weather(config.weather.townid, "metric")
@@ -37,36 +76,104 @@ def get_weather():
     return temp
 
 
-def send_to_display(pil_image):
+def send_to_display(pil_image: Image):
     """
     Send Data to Display
     """
     epd = epd7in5_V2.EPD()
-    # epd.Clear()
-    log.info("Initialising the display...")
+    log.info("Initializing the display...")
     epd.init()
     epd.display(epd.getbuffer(pil_image))
     log.info("Sending Display to Sleep")
     epd.sleep()
 
 
+def send_to_server(pil_image: Image):
+    """
+    Send to a server
+    """
+    # Define the API endpoint URL
+    url = "http://192.168.0.50:8000/upload"
+    epd = epd7in5_V2.EPD()
+    data_bytes = bytes(epd.getbuffer(pil_image))
+
+    # Send the bytes to the API
+    headers = {"Content-Type": "application/octet-stream"}
+    response = httpx.post(url, content=data_bytes, headers=headers)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        file_size = response_data.get("file_size")
+        log.info(f"File size: {file_size} bytes")
+    else:
+        raise HTTPException(
+            status_code=500, detail="Failed to send the bytearray to the server"
+        )
+
+
+def is_within_update_hours():
+    """
+    Don't need to update in middle of night
+    """
+    now = datetime.datetime.now()
+    return (
+        dashboard_update_enabled_hours[0]
+        <= now.hour
+        < dashboard_update_enabled_hours[1]
+    )
+
+
+def run_dashboard_update():
+    """
+    Run dashboard update if between two hours
+    """
+    if not is_within_update_hours():
+        log.info("Dashboard update disabled at this hour")
+        return
+    rail_client = NationalRail.NationalRail(config.tokens.national_rail)
+    pil_image = Pillow.render_pillow_dashboard(
+        rail_nb=rail_client.get_departures(
+            4, config.stations.northbound_from, config.stations.northbound_to
+        ),
+        rail_sb=rail_client.get_departures(
+            4, config.stations.southbound_from, config.stations.southbound_to
+        ),
+        temperature_data=get_weather(),
+    )
+
+    log.info("Displaying image and saving preview.png")
+    pil_image.save("preview.png")
+    # send_to_display(pil_image)
+    send_to_server(pil_image)
+
+
+@app.post("/update")
+async def update_dashboard(background_tasks: BackgroundTasks):
+    """
+    Endpoint to manually trigger the dashboard update
+    """
+    background_tasks.add_task(run_dashboard_update)
+    return {"message": "Dashboard update scheduled"}
+
+
+@app.post("/config")
+async def update_config(
+    new_config: UpdateConfig, api_config: APIConfig = Depends(get_apiconfig)
+):
+    """
+    Endpoint to update the dashboard update frequency
+    """
+    api_config.update_interval = datetime.timedelta(minutes=new_config.interval_minutes)
+    return {"message": "Dashboard update frequency updated"}
+
+
 def main():
     """Program Entrypoint"""
     try:
-        rail_client = NationalRail.NationalRail(config.tokens.national_rail)
-        pil_image = Pillow.render_pillow_dashboard(
-            rail_nb=rail_client.get_departures(
-                4, config.stations.northbound_from, config.stations.northbound_to
-            ),
-            rail_sb=rail_client.get_departures(
-                4, config.stations.southbound_from, config.stations.southbound_to
-            ),
-            temperature_data=get_weather(),
-        )
+        run_dashboard_update()  # Run the dashboard update initially
 
-        log.info("Displaying image and saving preview.png")
-        pil_image.save("preview.png")
-        send_to_display(pil_image)
+        uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+
     except IOError as exception:
         log.info(exception)
 
@@ -76,4 +183,5 @@ def main():
         sys.exit()
 
 
-main()
+if __name__ == "__main__":
+    main()
