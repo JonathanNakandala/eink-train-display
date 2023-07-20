f9c53e9fb2eb48cc220df39067e1564822aa06c7
Jonathan Nakandala <jonathannakandala@gmail.com>
2023-07-20 21:35:18 +0100

    Use http server

--- api/render_webpage.py
+++ api/render_webpage.py
@@ -2,7 +2,9 @@
 Use Playwright to render page
 """
 import io
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
+from threading import Thread
 from playwright.sync_api import sync_playwright
 from PIL import Image
 from structlog import get_logger
@@ -10,27 +12,36 @@ from .utils import send_to_server
 
 log = get_logger()
 
+PORT = 8927
+
+
+def start_http_server(port, directory):
+    class Handler(SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=directory, **kwargs)
+
+    httpd = HTTPServer(("localhost", port), Handler)
+    httpd.serve_forever()
+
 
 def render_webpage() -> Image:
     """
     Create a browser instance that renders the page
     """
+    webpage_path = (
+        Path(__file__).resolve().parent / ".." / "render" / "svelte" / "build"
+    ).resolve()
+    server_thread = Thread(target=start_http_server, args=(PORT, webpage_path))
+    server_thread.daemon = True
+    server_thread.start()
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(args=["--disable-web-security"])
         context = browser.new_context(viewport={"width": 800, "height": 480})
         page = context.new_page()
 
-        file_path = (
-            Path(__file__).resolve().parent
-            / ".."
-            / "render"
-            / "svelte"
-            / "build"
-            / "index.html"
-        ).resolve()
-        log.info(f"Loading Webpage: {file_path}")
+        log.info("Loading Webpage")
         page.on("console", lambda msg: log.info(msg.text))
-        page.goto(f"file://{file_path}")
+        page.goto(f"http://localhost:{PORT}/index.html")
 
         def check_class_loaded():
             return page.evaluate(
